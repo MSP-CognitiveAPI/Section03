c1e1820c17abe2fa0d8db0c8425e1b2ed046822e
BumMo Koo <ksquareatm@gmail.com>
2016-11-08 18:07:25 +0900

    Added empty Xcode project.

---
+++ Realtime Video Emotion/Realtime Video Emotion/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Realtime Video Emotion
+//
+//  Created by BumMo Koo on 2016. 11. 8..
+//  Copyright © 2016년 BumMo Koo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
