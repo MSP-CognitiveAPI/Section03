0238849b700740b842528426e11e392d1f266827
BumMo Koo <ksquareatm@gmail.com>
2016-11-09 14:19:50 +0900

    Implemented user interface.

---
+++ Emotion/Emotion/CameraViewController.swift
@@ -0,0 +1,13 @@
+//
+//  CameraViewController.swift
+//  Emotion
+//
+//  Created by BumMo Koo on 2016. 11. 9..
+//  Copyright © 2016년 BumMo Koo. All rights reserved.
+//
+
+import UIKit
+
+class CameraViewController: UIViewController {
+    
+}
