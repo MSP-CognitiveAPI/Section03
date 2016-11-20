c4a0afe72da3d5cfd340ff3327c5e8a715f225fd
BumMo Koo <ksquareatm@gmail.com>
2016-11-20 11:40:43 +0900

    Removed implementations of demo methods.

--- Emotion/Emotion/Demo_HistoryViewController.swift
+++ Emotion/Emotion/Demo_HistoryViewController.swift
@@ -14,24 +14,7 @@ extension HistoryViewController {
         guard let photos = self.photos else { return }
         var completeCount = 0
         for photo in photos {
-            API.detectFaces(photo: photo, handler: { [weak self] (faces) in
-                completeCount += 1
-                if completeCount == photos.count {
-                    guard let results = self?.realm?.objects(IdentifiableFace.self) else { return }
-                    var faces = [IdentifiableFace]()
-                    for result in results {
-                        faces.append(result)
-                    }
-                    API.groupFaces(faces: faces, handler: { [weak self] (groups, messyGroup) in
-                        self?.groups = groups
-                        self?.messyGroup = messyGroup
-                        self?.facesGrouped = true
-                        DispatchQueue.main.async {
-                            self?.collectionView.reloadData()
-                        }
-                    })
-                }
-            })
+            
         }
     }
 }
--- Emotion/Emotion/Demo_PhotoViewController.swift
+++ Emotion/Emotion/Demo_PhotoViewController.swift
@@ -10,16 +10,6 @@ import UIKit
 
 extension PhotoViewController {
     func handle(image: UIImage) {
-        API.requestEmotions(image: image, handler: { [weak self] faces in
-            guard let faces = faces else { return }
-            let face = faces.first
-            
-            self?.updateEmotionLabel(emotion: face?.emotion)
-            
-            let markedImage = image.mark(faces: faces)
-            self?.updateImageView(image: markedImage)
-            
-            self?.saveResult(image: image, faces: faces)
-        })
+        
     }
 }
